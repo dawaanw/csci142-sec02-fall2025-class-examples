63fcec100f463eb9b3e57a1d1b3eee4bff050f20
dawaanw <dwilliams30@stetson.edu>
2025-09-23 15:10:38 -0400

    Changes to test package

--- FiguresBetter/src/shapes/Main.java
+++ FiguresBetter/src/shapes/Main.java
@@ -1,12 +1,13 @@
 package shapes;
 
 import java.awt.Color;
+import java.awt.Point;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Square square1 = new Square();
-		Square square2 = new Square(15.0, Color.RED);
+		Square square2 = new Square(15.0, Color.RED, new Point(0,0));
 		//Shape shape = new Shape(Color.BLACK);
 		
 		System.out.println(square1);
